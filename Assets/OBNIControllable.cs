78eae60bd6ddfc254ae17ed9e8adef728b1181fb
AlexBourgeois <abourgeois27380@gmail.com>
2018-01-21 22:41:14 +0100

    Added OCF

---
+++ Assets/OBNIControllable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ImprovedPerlinNoiseProject;
+using UnityEngine;
+
+public class OBNIControllable : Controllable {
+
+    [OSCProperty]
+    [Range(0.0f, 20f)]
+    public float m_frequency = 10.0f;
+
+    [OSCProperty]
+    [Range(0.0f, 10f)]
+    public float m_lacunarity = 2.0f;
+
+    [OSCProperty]
+    [Range(0.0f, 3.0f)]
+    public float m_gain = 0.5f;
+
+    [OSCProperty]
+    [Range(0, 10)]
+    public int octave = 1;
+
+    [OSCProperty]
+    public int m_seed = 0;
+
+    public NoiseController noiseController;
+	
+	// Update is called once per frame
+	public override void Update ()
+	{
+        base.Update();
+
+	    noiseController.m_seed = m_seed;
+	    noiseController.m_frequency = m_frequency;
+	    noiseController.m_gain = m_gain;
+	    noiseController.m_lacunarity = m_lacunarity;
+	}
+}
---
+++ Assets/Plugins/SharedTexture/Spout/Scripts/InvertCamera.cs
@@ -0,0 +1,38 @@
+﻿//http://docs.unity3d.com/412/Documentation/ScriptReference/Camera.OnPreCull.html
+using UnityEngine;
+using System.Collections;
+
+namespace Spout{
+
+	[RequireComponent (typeof(Camera))]
+	[ExecuteInEditMode]
+	public class InvertCamera : MonoBehaviour {
+		private Camera camera;
+		void Start ()
+		{
+		    camera = GetComponent<Camera>();
+		}
+		
+		void OnPreCull () {
+		    if (camera.activeTexture != null)
+		    {
+		        GetComponent<Camera>().ResetWorldToCameraMatrix();
+		        GetComponent<Camera>().ResetProjectionMatrix();
+		        GetComponent<Camera>().projectionMatrix = GetComponent<Camera>().projectionMatrix *
+		                                                  Matrix4x4.Scale(new Vector3(1, -1, 1));
+		    }
+		}
+		
+		void OnPreRender () {
+		    if (camera.activeTexture != null)
+		        GL.invertCulling = true;
+		}
+		
+		void OnPostRender () {
+		    if (camera.activeTexture != null)
+                GL.invertCulling = false;
+		}
+		
+	}
+
+}
